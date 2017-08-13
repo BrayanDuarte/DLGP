9f4fe81ec358bcf0e9a90d7419ed1a820dd152c0
bduarte <bduarte1994@yahoo.com>
2017-08-13 12:42:41 -0600

    Primer commit desde la consolo ubuntu, incluye todos los archivos del proyecto

---
+++ application/views/footer.php
@@ -0,0 +1,29 @@
+ <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Diplomado en Liderazgo y Gerencia Política 2017</p>
+                    <p>Sitio Web creado por Ing. Bryam Duarte</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script type="text/javascript" src="<?php echo base_url("bootstrap/js/jquery.js"); ?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script type="text/javascript" src="<?php echo base_url("bootstrap/js/bootstrap.min.js"); ?>"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
